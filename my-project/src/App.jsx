75664e501e2aade1aecaff628a24de5f58f2f443
Davi Marques <dmo3@cesar.school>
2025-06-30 14:12:53 -0300

    [Feat] - first hero-section

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,15 @@
 import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
-    <NavBar></NavBar>
+    <>
+      <NavBar></NavBar>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection></HeroSection>
+      </div>
+    </>
+    
   )
 }
 
