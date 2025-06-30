c693ee69f36a8522b7299058bbe795cbd846d8bf
Davi Marques <dmo3@cesar.school>
2025-06-30 14:28:26 -0300

    [Feat] - second hero-section

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <NavBar></NavBar>
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection></HeroSection>
+        <FeatureSection></FeatureSection>
       </div>
     </>
     
