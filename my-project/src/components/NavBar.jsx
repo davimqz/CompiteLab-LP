4548c0b61cb5d362092d42d1b350f4c3acc86303
Davi Marques <dmo3@cesar.school>
2025-06-30 13:03:04 -0300

    [Init] - Initial commit

---
+++ my-project/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+        <div className="container px-4 mx-auto relative text-">
+            <div className="flex justify-center items-center">
+                <div className="flex items-center flex-shrink-0">
+                    <img src={logo} alt="" />
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
